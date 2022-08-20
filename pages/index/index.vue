727e07ba43a594ff13c87dcb46a218c9e546b3db
sunlee <303001274@qq.com>
2022-08-20 10:44:38 +0800

    init: init

---
+++ pages/index/index.vue
@@ -0,0 +1,74 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<button open-type="getUserInfo" @getuserinfo="login">登录</button>
+	</view>
+</template>
+
+<script setup>
+const login = async () => {
+	uni.showModal({
+		title: '温馨提示',
+		content: '亲，授权微信登录后才能正常使用小程序功能',
+		success(res) {
+			if (res.confirm) {
+				uni.getUserProfile({
+					desc: '注册用户信息使用',
+					lang: 'zh_CN',
+					success(res) {
+						console.log('res', res)
+						uni.login({
+							provider: 'weixin',
+							success(loginRes) {
+								console.log('loginRes.authResult', loginRes)
+								uniCloud.callFunction({
+									name: 'user',
+									data: {
+										code: loginRes.code,
+										avatarUrl: res.userInfo.avatarUrl,
+										gender: res.userInfo.gender,
+										nickName: res.userInfo.nickName
+									}
+								}).then(res => {})
+							}
+						})
+					}
+				});
+			} else {
+				uni.showToast({
+					title: '您取消了授权',
+					duration: 2000
+				})
+			}
+		}
+	});
+};
+</script>
+
+<style>
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+.logo {
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
+}
+
+.text-area {
+	display: flex;
+	justify-content: center;
+}
+
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
+</style>
---
+++ uniCloud-aliyun/cloudfunctions/user/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const appId = 'wx57034c482655bdc5'
+const secret = '3e1f18470227538fe6dc6f328b5782c0'
+
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+	const {
+		code,
+		avatarUrl,
+		gender,
+		nickName
+	} = event
+	const URL =
+		`https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${secret}&js_code=${code}&grant_type=authorization_code`
+	const requestOptions = {
+		method: 'GET',
+		dataType: 'json'
+	}
+	const res = await uniCloud.httpclient.request(URL, requestOptions)
+	console.log('res', res)
+
+	//返回数据给客户端
+	return event
+};
